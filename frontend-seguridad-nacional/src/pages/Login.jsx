6eb11a151c34a85017e4652e9924befb4127030d
Daniel Stiven Umaña <danikina43@gmail.com>
2026-03-12 23:59:32 -0500

    Implementación selección de roles en login

--- frontend-seguridad-nacional/src/pages/Login.jsx
+++ frontend-seguridad-nacional/src/pages/Login.jsx
@@ -4,19 +4,25 @@ function Login({ onLogin }) {
 
   const [usuario, setUsuario] = useState("");
   const [password, setPassword] = useState("");
+  const [rol, setRol] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (usuario === "admin" && password === "1234") {
-      onLogin();
-    } else {
-      alert("Credenciales incorrectas");
+    if(usuario && password && rol){
+      onLogin({
+        nombre: usuario,
+        rol: rol
+      });
+    }else{
+      alert("Complete todos los campos");
     }
   };
 
   return (
-    <div style={{ padding: "20px" }}>
+
+    <div style={{padding:"40px"}}>
+
       <h2>Inicio de sesión</h2>
 
       <form onSubmit={handleSubmit}>
@@ -25,27 +31,44 @@ function Login({ onLogin }) {
           type="text"
           placeholder="Usuario"
           value={usuario}
-          onChange={(e) => setUsuario(e.target.value)}
+          onChange={(e)=>setUsuario(e.target.value)}
         />
 
-        <br /><br />
+        <br/><br/>
 
         <input
           type="password"
           placeholder="Contraseña"
           value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e)=>setPassword(e.target.value)}
         />
 
-        <br /><br />
+        <br/><br/>
+
+        <label>Rol</label>
+
+        <select
+          value={rol}
+          onChange={(e)=>setRol(e.target.value)}
+        >
+          <option value="">Seleccione</option>
+          <option value="admin">Administrador</option>
+          <option value="supervisor">Supervisor</option>
+          <option value="guardia">Guardia</option>
+        </select>
+
+        <br/><br/>
 
         <button type="submit">
           Ingresar
         </button>
 
       </form>
+
     </div>
+
   );
+
 }
 
 export default Login;
